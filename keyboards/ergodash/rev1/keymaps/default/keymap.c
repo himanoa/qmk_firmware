c3888b07e4e7c61908fb6295df2d26c8fa898fdc
himanoa <matsunoappy@gmail.com>
2020-07-06 17:21:32 +0900

    Ctrlきーを親指で押せるように変更

--- keyboards/ergodash/rev1/keymaps/default/keymap.c
+++ keyboards/ergodash/rev1/keymaps/default/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_MINS,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL, \
-    KC_GRV, KC_LALT, KC_LGUI, KC_LGUI,            RAISE,   KC_SPC ,KC_LSFT,         KC_ESC,KC_ENT , LOWER,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_GRV, KC_LGUI, KC_LALT, KC_LCTL,            RAISE,   KC_SPC ,KC_LSFT,         KC_ESC,KC_ENT , LOWER,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   /* Lower
