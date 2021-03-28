8702ef92a40ab7c2f12e6fd86dc4d93333923fb8
himanoa <matsunoappy@gmail.com>
2021-03-29 00:16:23 +0900

    WIP

--- keyboards/ergodash/rev1/keymaps/default/keymap.c
+++ keyboards/ergodash/rev1/keymaps/default/keymap.c
@@ -75,7 +75,7 @@ KC_TAB,  KC_4,    KC_5,    KC_6,    KC_DOT,  KC_SLSH,    KC_DEL ,               
    */
   [_RAISE] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-    KC_TILD, KC_EXLM, KC_AT,   KC_END,  KC_COLN,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_LT, KC_GT, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
+    KC_TILD, KC_EXLM, KC_AT,   KC_END,  KC_COLN,  KC_SCLN, KC_UNDS,                        KC_PLUS, KC_LT, KC_GT, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_TAB,  KC_HOME, KC_2,    KC_3,    KC_DOT,   KC_SLSH,    KC_DEL ,                        KC_SCLN, KC_QUOT,    KC_SLSH,    KC_BSLS,    KC_L,    KC_COLN, KC_DQT , \
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_COMM,  KC_BSLS,    KC_SPC ,                        KC_ENT , KC_COMM,    KC_DOT,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
     KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC ,KC_DEL,         KC_BSPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
