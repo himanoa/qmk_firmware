f6fdc593327b99aeea5592db6e4c660fc22b68ef
himanoa <matsunoappy@gmail.com>
2021-03-30 18:21:31 +0900

    WIP

--- keyboards/ergodash/rev1/keymaps/default/keymap.c
+++ keyboards/ergodash/rev1/keymaps/default/keymap.c
@@ -7,13 +7,15 @@
 #define _ADJUST 16
 
 enum custom_keycodes {
-  ARROW_FUNCTION,
+  ARROW_FUNCTION_HYPHEN,
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
+  ARROW_FUNCTION_EQUAL
 };
 
+#define EISU LALT(KC_GRV)
 #define EISU LALT(KC_GRV)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -55,10 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
     KC_TILD, KC_1,    KC_2,    KC_3,    KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_LPRN, KC_RPRN, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-KC_TAB,  KC_4,    KC_5,    KC_6,    KC_DOT,  KC_SLSH,    KC_DEL ,                         KC_DQT, KC_LBRC,  KC_RBRC, KC_QUOT,    KC_L,    KC_COLN, KC_DQT , \
-    KC_LSFT, KC_7,    KC_8,    KC_9,    KC_0,    KC_0,    KC_SPC ,                        KC_1 , KC_LCBR,   KC_RCBR, KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC ,KC_DEL,         KC_BSPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_TAB,  KC_4,    KC_5,    KC_6,    KC_DOT,  KC_SLSH, KC_DEL ,                        KC_DQT,  KC_LBRC,  KC_RBRC, KC_QUOT,  KC_L,    KC_COLN, KC_DQT , \
+    KC_LSFT, KC_7,    KC_8,    KC_9,    KC_0,    KC_0,    KC_SPC ,                        KC_1 ,   KC_LCBR,  KC_RCBR, KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
+    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC ,KC_DEL,         ARROW_FUNCTION_HYPHEN,ARROW_FUNCTION_EQUAL , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
+
 
   /* Raise
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -77,8 +80,8 @@ KC_TAB,  KC_4,    KC_5,    KC_6,    KC_DOT,  KC_SLSH,    KC_DEL ,               
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
     KC_TILD, KC_QUES, KC_PIPE,   KC_END,  KC_COLN,  KC_SCLN, KC_UNDS,                        KC_PLUS, KC_LT, KC_GT, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_TAB,  KC_HOME, KC_LT,    KC_GT,    KC_DOT,   KC_SLSH,    KC_DEL ,                        KC_SCLN, KC_QUOT,    KC_SLSH,    KC_BSLS,    KC_L,    KC_COLN, KC_DQT , \
-    KC_LSFT, KC_CIRC,KC_AMPR,    KC_DQT,    KC_COMM,  KC_BSLS,    KC_SPC ,                        KC_ENT , KC_COMM,    KC_DOT,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC ,KC_DEL,         KC_BSPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_LSFT, KC_CIRC,KC_AMPR,    KC_DQT,    KC_COMM,  KC_BSLS,   KC_SPC ,                        KC_ENT , KC_COMM,    KC_DOT,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
+    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC ,KC_DEL,         ARROW_FUNCTION_EQUAL,ARROW_FUNCTION_HYPHEN , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
   /* Adjust
@@ -114,9 +117,15 @@ void persistent_default_layer_set(uint16_t default_layer) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case ARROW_FUNCTION:
+    case ARROW_FUNCTION_EQUAL:
       if (record->event.pressed) {
-          SEND_STRING("() => {}");
+          SEND_STRING("=>");
+      }
+      return false;
+      break;
+    case ARROW_FUNCTION_HYPHEN:
+      if (record->event.pressed) {
+          SEND_STRING("->");
       }
       return false;
       break;
